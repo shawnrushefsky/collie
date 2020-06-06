c40d49df57e9a5f43776e890064165f4ba4cb989
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-06-06 16:16:46 -0500

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const TerserPlugin = require('terser-webpack-plugin')
-const webpack = require('webpack');
 module.exports = {
   mode: 'development',
   entry: "./src/index.js",
@@ -17,7 +16,7 @@ module.exports = {
   },
   target: 'node',
   output: {
-    filename: 'bundle.js',
+    filename: 'index.js',
     libraryTarget: 'umd',
     globalObject: 'this'
   }
